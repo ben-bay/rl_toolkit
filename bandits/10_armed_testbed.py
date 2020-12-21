408cf5880db45136c46bfa200a150725cdbe0c34
Benjamin Bay <benbayml@gmail.com>
2020-12-20 17:39:11 -0700

    adding thompson sampling

--- bandits/10_armed_testbed.py
+++ bandits/10_armed_testbed.py
@@ -4,6 +4,7 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import beta
 
 """
 TODO
@@ -21,12 +22,12 @@ def do_action(a, testbed):
     return np.random.normal(testbed[a], 1)
 
 
-def e_greedy(test_bed, n_timesteps, epsilon=0, optimism=0):
+def e_greedy(test_bed, horizon, epsilon=0, optimism=0):
     # with sample averages
     rewards = []
     action_values = np.zeros((len(test_bed))) + optimism
     action_counts = np.zeros((len(test_bed)))
-    for step in range(1, n_timesteps + 1):
+    for step in range(1, horizon + 1):
         if random.random() > epsilon:
             action = np.argmax(action_values)
         else:
@@ -34,36 +35,36 @@ def e_greedy(test_bed, n_timesteps, epsilon=0, optimism=0):
         reward = do_action(action, test_bed)
         rewards.append(reward)
         action_counts[action] += 1.0
-        if step < n_timesteps - 1:
+        if step < horizon - 1:
             action_values[action] = action_values[action] + (
                 1 / action_counts[action]
             ) * (reward - action_values[action])
     return rewards
 
 
-def ucb(test_bed, n_timesteps, c):
+def ucb(test_bed, horizon, c):
     rewards = []
     action_values = np.zeros((len(test_bed)))
     action_counts = np.zeros((len(test_bed)))
-    for step in range(1, n_timesteps + 1):
+    for step in range(1, horizon + 1):
         action = np.argmax(action_values + (c * np.sqrt(np.log(step) / action_counts)))
         reward = do_action(action, test_bed)
         rewards.append(reward)
         action_counts[action] += 1.0
-        if step < n_timesteps - 1:
+        if step < horizon - 1:
             action_values[action] = action_values[action] + (
                 1 / action_counts[action]
             ) * (reward - action_values[action])
     return rewards
 
 
-def gradient(test_bed, n_timesteps, step_size, baseline=True):
+def gradient(test_bed, horizon, step_size, baseline=True):
     rewards = []
     action_preferences = np.zeros((len(test_bed)))
     action_counts = np.zeros((len(test_bed)))
     action_probabilities = np.zeros((len(test_bed))) + (1.0 / len(test_bed))
     mean_reward = 0
-    for step in range(1, n_timesteps + 1):
+    for step in range(1, horizon + 1):
         rand = random.random()
         prob_sum = 0
         for a, prob in enumerate(action_probabilities):
@@ -76,7 +77,7 @@ def gradient(test_bed, n_timesteps, step_size, baseline=True):
             # mean_reward = mean_reward + (1.0 / step) * (reward - mean_reward)
             mean_reward = np.mean(rewards)
         action_counts[action] += 1.0
-        if step < n_timesteps - 1:
+        if step < horizon - 1:
             action_preferences[action] = action_preferences[action] + step_size * (
                 reward - mean_reward
             ) * (1 - action_probabilities[action])
@@ -94,10 +95,44 @@ def gradient(test_bed, n_timesteps, step_size, baseline=True):
     return rewards
 
 
+def thompson_sampling(test_bed, horizon):
+    def select_arm(a, b, beta_params):
+        beta_params = zip(a, b)
+        # Perform random draw for all arms based on their params (a,b)
+        all_draws = [beta.rvs(i[0], i[1], size=1) for i in beta_params]
+        # return index of arm with the highest draw
+        return all_draws.index(max(all_draws))
+
+    # Choose to update chosen arm and reward
+    def update(counts, values, a, b, chosen_arm, reward):
+        # update counts pulled for chosen arm
+        counts[chosen_arm] = counts[chosen_arm] + 1
+        n = counts[chosen_arm]
+        
+        # Update average/mean value/reward for chosen arm
+        value = values[chosen_arm]
+        new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
+        values[chosen_arm] = new_value
+        
+        # Update a and b... 
+        # a is based on total counts of rewards of arm
+        a[chosen_arm] = a[chosen_arm] + reward
+        # b is based on total counts of failed rewards on arm
+        b[chosen_arm] = b[chosen_arm] + (1-reward)        
+        return counts, values, a, b
+
+    n_arms = len(test_bed)
+    counts = np.zeros(n_arms)
+    values = np.zeros(n_arms)
+    a = np.ones(n_arms)
+    b = np.ones(n_arms)
+
+
+
 def process_args(args):
     n_arms = args.narms
     n_runs = args.nruns
-    n_timesteps = args.ntimesteps
+    horizon = args.ntimesteps
     alg_specs = []
     # algortihm tuple: function, plot label, kwargs
     alg_specs.append((e_greedy, "greedy", {}))
@@ -110,13 +145,13 @@ def process_args(args):
     n_algs = len(alg_specs)
     algorithm_runs = []
     for i in range(n_algs):
-        algorithm_runs.append(np.ndarray((n_runs, n_timesteps)))
+        algorithm_runs.append(np.ndarray((n_runs, horizon)))
     for run_i in range(n_runs):
         test = testbed(n_arms, args.mean, args.variance)
         rewards = []
         for alg_i in range(n_algs):
             alg = alg_specs[alg_i]
-            rewards.append(alg[0](test, n_timesteps, **alg[2]))
+            rewards.append(alg[0](test, horizon, **alg[2]))
         for alg_i in range(n_algs):
             algorithm_runs[alg_i][run_i] = rewards[alg_i]
 
@@ -124,7 +159,7 @@ def process_args(args):
 
     for alg_i in range(n_algs):
         ax.plot(
-            np.arange(n_timesteps),
+            np.arange(horizon),
             algorithm_runs[alg_i].mean(axis=0),
             label=alg_specs[alg_i][1],
         )
